bac2d282149bf59ce8cfff9f7f90e33e360cc44d
nocmok <nyusafonov@edu.hse.ru>
2021-06-06 22:43:06 +0300

    count unique function

--- algo/strings/PrefixFun/PrefixFun.java
+++ algo/strings/PrefixFun/PrefixFun.java
@@ -2,6 +2,11 @@ public class PrefixFun {
 
     public static int[] prefixFunction(String str) {
         int[] pi = new int[str.length()];
+        prefixFunction(str, pi);
+        return pi;
+    }
+
+    public static void prefixFunction(String str, int[] pi) {
         for (int i = 1; i < str.length(); ++i) {
             int currPi = pi[i - 1];
             while (currPi > 0 && str.charAt(currPi) != str.charAt(i)) {
@@ -11,6 +16,19 @@ public class PrefixFun {
                 pi[i] = currPi + 1;
             }
         }
-        return pi;
+    }
+
+    public static int countUniqueSubstrings(String str) {
+        int unique = 0;
+        int[] pi = new int[str.length()];
+        for (int i = 1; i <= str.length(); ++i) {
+            prefixFunction(str.substring(str.length() - i), pi);
+            int max = 0;
+            for (int j = 0; j < i; ++j) {
+                max = Integer.max(max, pi[j]);
+            }
+            unique += (i - max);
+        }
+        return unique;
     }
 }
